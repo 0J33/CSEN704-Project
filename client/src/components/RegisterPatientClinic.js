9624e4969df09bd3b2055048fc53f940fcbd74fd
Ojee <omargamal.02.11@gmail.com>
2024-03-04 21:30:47 +0200

    fix ui

--- client/src/components/RegisterPatientClinic.js
+++ client/src/components/RegisterPatientClinic.js
@@ -2,6 +2,21 @@ import { useState } from "react";
 import { Navbar } from "react-bootstrap";
 
 function RegisterPatientClinic() {
+
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
     const [credentials, setCredentials] = useState({
         username: '',
         password: '',
