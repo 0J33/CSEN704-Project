0ff1828e56abe5641047ce5540180d1c275b1633
Ojee <omargamal.02.11@gmail.com>
2024-03-04 21:37:18 +0200

    fix ui

--- client/src/components/RegisterPharmacist.js
+++ client/src/components/RegisterPharmacist.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Navbar } from "react-bootstrap";
 
 function RegisterPharmacist() {
